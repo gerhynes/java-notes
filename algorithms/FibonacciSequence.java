2ac7ba8cf7276ab6e0bed52a67a1001e92a77956
Gerard Hynes <gkhynes@gmail.com>
2021-11-24 17:16:01 +0000

    Update FibonacciSequence

--- algorithms/FibonacciSequence.java
+++ algorithms/FibonacciSequence.java
@@ -2,26 +2,30 @@ import java.util.Scanner;
 
 public class FibonacciSequence {
     public static void main(String[] args) {
-        // nth number to calculate Fibonacci sequence to
-//        int n = 12;
-
         System.out.println("Enter a number (n) to calculate the Fibonacci sequence to the nth number");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
+        // Print Fibonacci sequence to nth number
         System.out.println("Fibonacci sequence up to " + n + "th number without recursion or functions");
         for(int i = 0; i < n; i++){
             System.out.print(iterativeFibonacci(i) + " ");
         }
 
+        // Print line to separate results
+        System.out.println();
+
+        // Print Fibonacci sequence to nth number
         System.out.println("Fibonacci sequence up to " + n + "th number using recursion");
         for(int i = 0; i < n; i++){
             System.out.print(recursiveFibonacci(i) + " ");
-            // Print newline character after printing sequence
-            if(i == n-1) System.out.printf("%n");
         }
+
+        // Close scanner to prevent memory leak
+        scanner.close();
     }
 
+    // Calculate nth number in Fibonacci sequence without recursion
     public static int iterativeFibonacci(int n){
         int prev = 0;
         int next = 1;
@@ -33,11 +37,12 @@ public class FibonacciSequence {
         return prev;
     }
 
+    // Calculate nth number in Ficonacci sequence using recursion
     public static int recursiveFibonacci(int n){
-        if(n == 0){ // First number in Fibonacci sequence is 0
-            return 0;
-        } else if (n == 1 || n == 2){ // 2nd and 3rd number in sequence are 1 and 1
-            return 1;
+        if(n == 0){
+            return 0; // 1st number in Fibonacci sequence is 0
+        } else if (n <= 2){
+            return 1; // 2nd and 3rd number in sequence are 1 and 1
         } else {
             // Recursively call the function
             return recursiveFibonacci(n-2) + recursiveFibonacci(n-1);
