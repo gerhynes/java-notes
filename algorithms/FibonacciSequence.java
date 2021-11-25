643c9a82e5282cc9022e0989c3693a7ea4a7e85b
Gerard Hynes <gkhynes@gmail.com>
2021-11-25 16:03:31 +0000

    Update MergeSort and Fibonacci

--- algorithms/FibonacciSequence.java
+++ algorithms/FibonacciSequence.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class FibonacciSequence {
     public static void main(String[] args) {
-        System.out.println("Enter a number (n) to calculate the Fibonacci sequence to the nth number");
+        // Initialize scanner object
         Scanner scanner = new Scanner(System.in);
+
+        // Take in integer n
+        System.out.println("Enter a number (n) to calculate the Fibonacci sequence to the nth number:");
         int n = scanner.nextInt();
 
         // Print Fibonacci sequence to nth number
@@ -27,24 +30,26 @@ public class FibonacciSequence {
 
     // Calculate nth number in Fibonacci sequence without recursion
     public static int iterativeFibonacci(int n){
-        int prev = 0;
-        int next = 1;
+        // Each number in the sequence is the sum of the previous two numbers
+        int first = 0; // 1st number in Fibonacci sequence is 0
+        int second = 1; // 2nd number in Fibonacci sequence is 1
         for (int i = 0; i < n; ++i) {
-            int sum = prev + next;
-            prev = next;
-            next = sum;
+            int sum = first + second;
+            first = second;
+            second = sum;
         }
-        return prev;
+        return first;
     }
 
     // Calculate nth number in Ficonacci sequence using recursion
     public static int recursiveFibonacci(int n){
         if(n == 0){
             return 0; // 1st number in Fibonacci sequence is 0
-        } else if (n <= 2){
-            return 1; // 2nd and 3rd number in sequence are 1 and 1
+        } else if (n == 1){
+            return 1; // 2nd number in Fibonacci sequence is 1
         } else {
-            // Recursively call the function
+            // Recursively call the function for the 2 numbers immediately lower than n
+            // For example, recursiveFibonacci(5) = recursiveFibonacci(3) + recursiveFibonacci(4)
             return recursiveFibonacci(n-2) + recursiveFibonacci(n-1);
         }
     }
--- algorithms/MergeSort.java
+++ algorithms/MergeSort.java
@@ -2,23 +2,20 @@ import java.util.Scanner;
 
 class MergeSort {
     public static void main(String args[]) {
-        // Initialize array
-        int arr[] = { 4, 1, 7, 3, 12, 2, -1 };
+        // Set desired length of array
+        int arrLength = 10;
 
-//        // Set desired length of array
-//        int arrLength = 10;
-//
-//        // Initialize array
-//        int[] arr = new int[arrLength];
-//
-//        // Initialize scanner object
-//        Scanner scanner = new Scanner(System.in);
-//
-//        // Take in numbers from user and populate array
-//        System.out.println("Enter " + arrLength + " numbers to populate an array: ");
-//        for(int i = 0; i < arr.length; i++){
-//            arr[i] = scanner.nextInt();
-//        }
+        // Initialize array
+        int[] arr = new int[arrLength];
+
+        // Initialize scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Take in numbers from user and populate array
+        System.out.println("Enter " + arrLength + " numbers to populate an array: ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
 
         // Print unsorted array
         System.out.println("Unsorted Array");
@@ -33,30 +30,30 @@ class MergeSort {
     }
 
     // Sort arr[leftIndex...rightIndex] using mergesort
-    static void mergesort(int arr[], int leftIndex, int rightIndex)
+    static void mergesort(int arr[], int firstIndex, int lastIndex)
     {
         // Check if the array contains more than one element and is unsorted
-        if (leftIndex < rightIndex) {
+        if (firstIndex < lastIndex) {
             // Find the midpoint of the array
-            int mid = (leftIndex + rightIndex)/2;
+            int mid = (firstIndex + lastIndex)/2;
 
             // Sort the left and right halves of the array
-            mergesort(arr, leftIndex, mid);
-            mergesort(arr, mid + 1, rightIndex);
+            mergesort(arr, firstIndex, mid);
+            mergesort(arr, mid + 1, lastIndex);
 
             // Merge the sorted halves
-            merge(arr, leftIndex, mid, rightIndex);
+            merge(arr, firstIndex, mid, lastIndex);
         }
     }
 
     // Merge two subarrays of arr
     // First subarray is arr[leftIndex...mid]
     // Second subarray is arr[mid+1...rightIndex]
-    static void merge(int arr[], int leftIndex, int mid, int rightIndex)
+    static void merge(int arr[], int firstIndex, int mid, int lastIndex)
     {
         // Find the lengths of the two subarrays to be merged
-        int len1 = mid - leftIndex + 1;
-        int len2 = rightIndex - mid;
+        int len1 = mid - firstIndex + 1;
+        int len2 = lastIndex - mid;
 
         // Create temporary arrays
         int leftArr[] = new int[len1];
@@ -64,7 +61,7 @@ class MergeSort {
 
         // Copy arr contents to temporary arrays
         for (int i = 0; i < len1; i++)
-            leftArr[i] = arr[leftIndex + i];
+            leftArr[i] = arr[firstIndex + i];
         for (int j = 0; j < len2; j++)
             rightArr[j] = arr[mid + 1 + j];
 
@@ -77,7 +74,7 @@ class MergeSort {
         int j = 0;
 
         // Initial index of merged subarray array
-        int k = leftIndex;
+        int k = firstIndex;
 
         // Add values to merged array in order
         while (i < len1 && j < len2) {
@@ -95,7 +92,7 @@ class MergeSort {
         }
 
         // ========================
-        // Copy any leftover values
+        // Copy any leftover value(s)
         // ========================
 
         // Copy remaining elements, if any, of leftArr into sorted array
@@ -120,4 +117,3 @@ class MergeSort {
         System.out.println();
     }
 }
-
